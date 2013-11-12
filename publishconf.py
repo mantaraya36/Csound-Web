3325b2e4567b3091e615b9f79e5ec9bddf02f0a5
Andres Cabrera <andres@lenovo-W530>
2013-11-12 14:14:52 -0800

    test theme

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://csound.sourceforge.net'
+#SITEURL = 'http://csound.sourceforge.net'
+SITEURL = 'http://mantaraya36.github.io/Csound-Web'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
